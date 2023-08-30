334bfd55fd09183addf7fdba4c73ad426d65efdd
Michael Bond <codemonkey8510@icloud.com>
2023-08-30 16:39:14 -0400

    Add project files.

---
+++ BlazorUnitedTest/Pages/Counter.razor.cs
@@ -0,0 +1,11 @@
+namespace BlazorUnitedTest.Pages;
+
+public partial class Counter
+{
+    private int currentCount = 0;
+
+    private void IncrementCount()
+    {
+        currentCount++;
+    }
+}
---
+++ BlazorUnitedTest/Pages/Weather.razor.cs
@@ -0,0 +1,33 @@
+namespace BlazorUnitedTest.Pages;
+
+public partial class Weather
+{
+    private static readonly string[] Summaries =
+    [
+        "Freezing",
+        "Bracing",
+        "Chilly",
+        "Cool",
+        "Mild",
+        "Warm",
+        "Balmy",
+        "Hot",
+        "Sweltering",
+        "Scorching",
+    ];
+
+    private WeatherForecast[]? forecasts;
+
+    protected override async Task OnInitializedAsync()
+    {
+        // Simulate retrieving the data asynchronously.
+        await Task.Delay(1000);
+        var startDate = DateOnly.FromDateTime(DateTime.Now);
+        forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = startDate.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        }).ToArray();
+    }
+}
